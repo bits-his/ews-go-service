b37a0d98b4cb74633dd6135088210b7071092c3a
salemzii <salemododa2@gmail.com>
2022-12-13 11:46:33 +0100

    consume alerts

--- main.go
+++ main.go
@@ -1,36 +1,8 @@
 package main
 
-import (
-	"github.com/salemzii/cygio/facebook"
-)
+import "github.com/salemzii/cygio/app"
 
 func main() {
 
-	facebook.PagePost("Hello Facebook from ews")
-
-	//app.ConsumeAlerts()
-	//messages.SendSmsTwilio()
-	//messages.SendSms("Early warning test projects", "08053503763")
-	//messages.SendBulkSms("This is a test disaster alert warning message, Kindly ignore. THERE IS A VOLCANO ERUPTION AT LUGBE. Visit https://www.brainstorm.ng Message sent By Dangana",
-	//[]string{"07031979192", "09129172628", "08168955444", "08065284720", "08080888788",
-	//"08053503763", "08133646976", "08065577665", "08033014093", "08038195815", "07035384184"})
-	//messages.SendMails("Early Warning/Early Response ALert Dispatcher", "This is a test disaster alert warning message, Kindly ignore. \n THE IS A VOLCANO ERUPTION AT LUGBE. \n Visit https://www.brainstorm.ng \nMessage sent By Dangana", []string{"salemododa2@gmail.com",
-	//"salem@brainstorm.ng", "ibagwai9@gmail.com", "maukankia@gmail.com", "ganygreen@gmail.com", "hamada131@gmail.com", "ibiauwal@gmail.com", "danjumadagana@gmail.com", "sglagosplus@gmail.com"})
-	//usmankibon@gmail.com, 07031979192
-
-	// okaforchris32@yahoo.com, 08168955444
-	// ngwabeche@gmail.com, 09129172628
-	// 08065284720, danjumadagana@gmail.com
-	// 08080888788, sglagosplus@gmail.com
-
-	//ConnectRMQ()
-	//streams.GetRules()
-	//streams.AddRules()
-	//streams.DeleteRules()
-	//streams.InitiateStream()
-	//SendNewsletterMail()
-
-	//twitter.GetUserByUserName("salemzii")
-	//twitter.DeleteTweet(1597287010863038465)
-	//messages.SendSms("hello zii", "")
+	app.ConsumeAlerts()
 }
