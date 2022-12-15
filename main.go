4c89eed0e18bec4297c736934f70af695f714bda
salemzii <salemododa2@gmail.com>
2022-12-15 11:08:20 +0100

    env conf

--- main.go
+++ main.go
@@ -16,12 +16,19 @@ func main() {
 	path, _ := os.Getwd()
 	err := godotenv.Load(filepath.Join(path, ".env"))
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		log.Println("Error loading .env file")
 	}
 
 	PRT := os.Getenv("PRT")
 	router := gin.Default()
 
+	router.GET("/welcome", welcome)
 	router.POST("/createalert", app.ReceiveAlert)
 	router.Run(fmt.Sprintf(":%s", PRT))
+}
+
+func welcome(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"msg": "Welcome to ews api",
+	})
 }
